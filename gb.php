7079b00c7337d91d542f309cf47e83f10521b87d
Cosmo Jim <cosmo@cosmo-jim.de>
2017-06-10 10:42:21 +0200

    .

--- gb.php
+++ gb.php
@@ -10,7 +10,7 @@
         <iframe src="http://www.guestbook-free.com/books3/cosmojim/s" height="90%" width="100%"></iframe>
         <iframe src="http://www.cosmo-jim.de/head.php" seamless width="100%" height="150" name="Cosmo-Head">
             <p>Ihr Browser kann leider keine eingebetteten Frames anzeigen: Sie können die eingebettete Seite über den folgenden Verweis 
-                aufrufen: <a href="http://www.cosmo-jim.de/index.html">SELFHTML</a> </p> 
+                aufrufen: <a href="http://www.cosmo-jim.de/index.html">Cosmo-Jim.de alt</a> </p> 
         
 <?php
 
