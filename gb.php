2bc34530236c1a2aa0cc8a3c6bd8b58f2e16b642
Cosmo Jim <cosmo@cosmo-jim.de>
2017-06-10 10:33:13 +0200

    Uploaded the webside

---
+++ gb.php
@@ -0,0 +1,22 @@
+<html>
+    <head>
+       <title>Cosmo´s G&auml;stebuch</title>
+
+        <link href="cosmo1.css" type="text/css" rel="stylesheet">
+        
+    </head>
+    <body>
+
+        <iframe src="head.php" height="10%" width="100%"></iframe>
+        <iframe src="http://www.guestbook-free.com/books3/cosmojim/s" height="90%" width="100%"></iframe>
+
+<?php
+
+
+
+
+
+?>
+
+</body>
+</html>
